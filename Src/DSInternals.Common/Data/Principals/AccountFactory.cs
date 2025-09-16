126c0f48b35c889be6a03111918feec9078ff7d6
Daniel Tarnu <danielt@contractor.knowbe4.com>
2025-09-16 22:04:32 +0300

    added new CreateAccount overload method

--- Src/DSInternals.Common/Data/Principals/AccountFactory.cs
+++ Src/DSInternals.Common/Data/Principals/AccountFactory.cs
@@ -12,6 +12,35 @@ namespace DSInternals.Common.Data
             // Check that the object is an account
             dsObject.ReadAttribute(CommonDirectoryAttributes.SamAccountType, out SamAccountType? accountType);
 
+            if (accountType != SamAccountType.User)
+                return null;
+
+            switch (accountType)
+            {
+                case SamAccountType.User:
+                    return new DSUser(dsObject, netBIOSDomainName, pek, propertySets);
+                case SamAccountType.Computer:
+                    return new DSComputer(dsObject, netBIOSDomainName, pek, propertySets);
+                case SamAccountType.Trust:
+                    return new DSAccount(dsObject, netBIOSDomainName, pek, propertySets);
+                default:
+                    // This object is not an account
+                    return null;
+            }
+        }
+
+        public static DSAccount CreateAccount(DirectoryObject dsObject, string netBIOSDomainName, DirectorySecretDecryptor pek,
+            SamAccountType samAccountType,
+            AccountPropertySets propertySets = AccountPropertySets.All)
+        {
+            // Validate the input.
+            Validator.AssertNotNull(dsObject, nameof(dsObject));
+
+            // Check that the object is an account
+            dsObject.ReadAttribute(CommonDirectoryAttributes.SamAccountType, out SamAccountType? accountType);
+
+            if(accountType != SamAccountType.User) return null;
+
             switch (accountType)
             {
                 case SamAccountType.User:
